97b03e3b1cd60a723b4d54ff9ed0f4c670d6cb52
Nehiomi <zarza.naomi@gmail.com>
2019-03-04 23:30:45 +0100

    Método Main final

--- src/alquilervehiculos/MisVehiculos.java
+++ src/alquilervehiculos/MisVehiculos.java
@@ -5,11 +5,12 @@
  */
 package alquilervehiculos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author naomi
  */
-
 public class MisVehiculos {
 
     public static void main(String[] args) {
@@ -22,7 +23,7 @@ public class MisVehiculos {
         easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
 
         // registro de los vehículos de la empresa
-        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda","Fabia", "Blanco", 90.0, false));
+        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
 
         easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
 
@@ -35,38 +36,59 @@ public class MisVehiculos {
         // imprime la relación de clientes de easydrive
         System.out.println("Imprime la relación de clientes de easydrive: ");
         easydrive.imprimirClientes();
-        
+
         // imprime el catálogo de vehículos de easydrive
         System.out.println("Imprime el catálogo de vehículos de easydrive: ");
         easydrive.imprimirVehiculos();
-       
+
         //Rellenar el array de vehículos de forma aleatoria
         System.out.println("Rellena el array de vehículos de easydrive de forma aleatoria: ");
         easydrive.rellenarVehiculos();
-        
+
         //Rellenar el array de clientes de forma aleatoria.
         System.out.println("Rellena el array de clientes de easydrive de manera aleatoria: ");
         easydrive.rellenarCLientes();
-        
+
         //Imprimir ambos arrays.
         System.out.println("Imprime clientes: ");
         easydrive.imprimirClientes();
         System.out.println("Imprime clientes: ");
         easydrive.imprimirVehiculos();
-        
-        //Ordenación burbuja de ambos arrays.
-        easydrive.ordenarCliente();
-        easydrive.ordenarVehiculo();
-        
-        //Imprimir
+
+        //Búsqueda binaria del cliente por nif.
+        easydrive.buscarCliente("Z7568991Y");
+
+        //Búsqueda binaria del vehículo por matrícula.
+        easydrive.buscarVehiculo("4070 DEP");
+
+        System.out.println();
+        //Busco el cliente concreto y el vehiculo concreto.
+        easydrive.buscarCliente("21358003L");
+        easydrive.buscarVehiculo("9326HLM");
+
         easydrive.imprimirClientes();
         easydrive.imprimirVehiculos();
 
-        //Búsqueda binaria del cliente por nif.
-        easydrive.buscarCliente("X5618927C");
-        
-        //Búsqueda binaria del vehículo por matrícula.
-         easydrive.buscarVehiculo("4080 TUR");
+        //Se alquila un vehiculo a un usuario
+        System.out.println();
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce tu NIF: ");
+        String nif = teclado.nextLine();
+        System.out.println("Introduce la matrícula del coche: ");
+        String matr = teclado.nextLine();
+        System.out.println("Introduce los días: ");
+        int dias = teclado.nextInt();
 
+        easydrive.alquilarVehiculo(matr, nif, dias);
+
+        System.out.println();
+        teclado.nextLine();
+        System.out.println("Introduce tu NIF: ");
+        nif = teclado.nextLine();
+        System.out.println(easydrive.buscarCliente(nif));
+
+        System.out.println("Introduce la matrícula del coche: ");
+        matr = teclado.nextLine();
+        System.out.println(easydrive.buscarVehiculo(matr));
     }
 }
